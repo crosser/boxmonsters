2621f952a9aa0117294d8acb1874db2dc37bec55
Eugene Crosser <Eugene.Crosser@ru.ibm.com>
2014-11-14 17:44:22 +0300

    some projectile rendering cleanup

--- Projectile.hs
+++ Projectile.hs
@@ -23,17 +23,18 @@ projectilesWire = (:[]) <$> (pure $ Projectile ((0,0,0.5),(0,0,0.1)))
 
 -- Rendering projectile.
 
-renderShaft :: Vec3 -> IO ()
-renderShaft (x, y, z) =
-  renderPrimitive Lines $ mapM_ vertex3d
-  [ (x, y, z), (x, y, z-hv) ]
+-- FIXME render shafts in the process of being reflected
+renderShaft :: Vec3 -> Bool -> IO ()
+renderShaft (x, y, z) outgoing = do
+  if outgoing then
+    color4d (0, 1, 1, 1)
+  else
+    color4d (1, 0, 1, 1)
+  renderPrimitive Lines $ mapM_ vertex3d 
+    [ (x, y, z), (x, y, z-(if outgoing then hv; else (-hv))) ]
 
 renderProjectile :: Size -> Projectile -> IO ()
 renderProjectile _ (Projectile (loc, (_, _, vz))) = do
   -- matrixMode $= Modelview 0
   -- loadIdentity
-  if vz > 0 then
-    color4d (0, 1, 1, 1)
-  else
-    color4d (1, 0, 1, 1)
-  renderShaft loc
+  renderShaft loc (vz > 0)
